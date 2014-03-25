f63d475c656462a6ce8307c056a310d77fc353df
adon <winy95battle@gmail.com>
2014-03-25 12:16:00 +0100

    Add double quote protection, print readable character

--- parser/srcs/parser.py
+++ parser/srcs/parser.py
@@ -5,16 +5,19 @@ __author__ = "Guieu Christophe, Tallot Adrien"
 __date__ = "21-03-2014"
 
 import os
+import sys
 
 
 def getKeyValue(line):
 
-    """Split the line and get the key and the value"""
+    """Split the line and get the key and the value
+        return a tuple wich contain the key and the value
+    """
     splitline = line.split("\t")
     key = splitline[0]
     value = splitline[2]
 
-    return (key, value)
+    return (key, str(value))
 
 
 def addDico(dico, key, value):
@@ -53,20 +56,56 @@ def isAdmisible(line):
         return True
 
 
+def removeCR(word):
+    """Remove ending carriage return
+    """
+    return str(word[0:-1])
+
+
+def protectChar(word):
+    """Protect Char in a word for example : מַכָּ"ם => מַכָּ\"ם"""
+    if "\"" in word:
+        tmp = list(word)
+        tmp[tmp.index("\"")] = "\\\""
+        word = ''.join(tmp)
+    return str(word)
+
+
+def parseValue(word):
+    word = removeCR(word)
+    word = protectChar(word)
+    return str(word)
+
+
 def parseFile(simplefile, dico, lng):
 
     """Parse a file and add it into the dictionary"""
     for line in simplefile:
         if not(isAdmisible(line)):
-            print(line)
             continue
         key, value = getKeyValue(line)
+        value = parseValue(value)
         value = (value, lng)
 
         addDico(dico, key, value)
 
 
+def parseSimpleFile(filename):
+    if not(os.path.isfile(filename)):
+        print("This file does not exist")
+        sys.exit(-1)
+    dico = {}
+    f = open(filename, 'r')
+    lng = os.path.basename(filename).split('.')[0].split('-')[2]
+    parseFile(f, dico, lng)
+
+    return dico
+
+
 def filesFromDir(directory):
+    if not(os.path.isdir(directory)):
+        print("This directory does not exist")
+        sys.exit(-1)
 
     """List all files from the given directory"""
     filenames = list([])
@@ -93,7 +132,11 @@ def parseFiles(directory):
 
 def main():
     """docstring for main"""
-    dico = parseFiles("../data/")
+    if sys.argv[1] == "-d":
+        dico = parseFiles(sys.argv[2])
+    if sys.argv[1] == "-f":
+        dico = parseSimpleFile(sys.argv[2])
+
     print(dico["00001740-n"])
 
 if __name__ == '__main__':
