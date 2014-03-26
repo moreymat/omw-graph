f34d3d6b6edf8af6a6f75265df6b768e1f017e15
adon <winy95battle@gmail.com>
2014-03-26 21:10:27 +0100

    Add doc in py file and create the directoryparser.py

---
+++ parser/srcs/directoryparser.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+"""
+Parse all file in a directory
+"""
+
+import os
+import sys
+from simpleFileParser import parseFile
+from simpleFileParser import getKey
+from simpleFileParser import getValue
+
+__author__ = 'Guieu Christophe, Tallot Adrien'
+__date__ = '26-03-2014'
+__version__ = '0.1'
+
+dico = {}
+
+
+def getDico():
+    """ return the dictionnary """
+    global dico
+    return dico
+
+
+def filesFromDir(directory):
+    """ list the file from a given directory
+    return a list of filename
+    """
+    files = list([])
+    for f in os.listdir(directory):
+        path = directory + f
+        files.append(path)
+    return files
+
+
+def addDico(data):
+    """ Add a value to a key in the dictionnary if the key exist,
+    else create the entry and set it to the value
+    """
+    global dico
+    for d in data:
+        key = getKey(d)
+        value = getValue(d)
+
+        if dico.get(key) is None:
+            dico[key] = [value]
+        else:
+            dico[key].append(value)
+
+
+def parseFiles(files):
+    """ parse all file in the filename list """
+    for f in files:
+        print(f)
+        data = parseFile(f)
+        addDico(data)
+
+
+def parseDirectory(directory):
+    """ check if the directory exist and
+    parse all the file from the directory """
+    if not(os.path.isdir(directory)):
+        print(directory + " : does not exist")
+        sys.exit(-1)
+    print ("========= Parse " + directory + " =========")
+    files = filesFromDir(directory)
+    parseFiles(files)
+
+
+def main(directory):
+    global dico
+
+    parseDirectory(directory)
+
+
+if __name__ == '__main__':
+    main('../data/')
--- parser/srcs/simpleFileParser.py
+++ parser/srcs/simpleFileParser.py
@@ -2,7 +2,7 @@
 #-*- coding: utf-8 -*-
 
 """
-Simple File Parser
+Simple tab file parser
 """
 
 import os
@@ -18,7 +18,18 @@ ccl = []
 deli = ''
 
 
+def getKey(t):
+    """ Return the key of a tuple"""
+    return t[0]
+
+
+def getValue(t):
+    """ return the value of a tuple"""
+    return t[1]
+
+
 def verifyFile(filename):
+    """ Verify the file """
     if filename == '':
         print('Error : put filename')
         sys.exit(-1)
@@ -28,6 +39,7 @@ def verifyFile(filename):
 
 
 def openFile(filename=''):
+    """ Open a file wich the name is given"""
     verifyFile(filename)
     f = open(filename, 'r')
 
@@ -35,29 +47,39 @@ def openFile(filename=''):
 
 
 def removeCR(word):
-    return word[0:-1]
+    """ Remove ending carriage return"""
+    return str(word[0:-1])
 
 
 def protectChar(word):
+    """ protect specify char with a \ """
     if '"' in word:
         lw = list(word)
         lw[lw.index('"')] = '\\"'
         word = ''.join(lw)
-    return word
+    return str(word)
 
 
 def parseWord(word):
+    """ Remove the carriage return and protect the char
+        return the word as a string
+    """
     word = removeCR(word)
     word = protectChar(word)
     return str(word)
 
 
 def splitLine(line):
+    """ Split the line with the given delimitor """
     global deli
     return line.split(deli)
 
 
 def parseLine(line):
+    """ Check if the line is commented
+    if not extract the key and the value from the line and
+    return it as a tuple (key, value)
+    """
     if line[0] in ccl:
         return
     else:
@@ -65,12 +87,14 @@ def parseLine(line):
         if not(sl[1] in dtl):
             return
         word = parseWord(str(sl[2]))
-        return (sl[0], str(word))
+        key = sl[0]
+        return (key, str(word))
 
 
 def setVariable(delimitor='\t',
                 commentcharlist=['#'],
                 datatypelist=['lemma']):
+    """ Set global virables """
     global dtl
     global ccl
     global deli
@@ -84,12 +108,15 @@ def parseFile(filename='', delimitor='\t',
               commentcharlist=['#'],
               datatypelist=['lemma']):
 
+    """ parse a file and return the data extract in a list of tuples """
+
     data = []
     setVariable(delimitor, commentcharlist, datatypelist)
     f = openFile(filename)
     for line in f:
         kv = parseLine(line)
-        data.append(kv)
+        if kv is not None:
+            data.append(kv)
 
     return data
 
