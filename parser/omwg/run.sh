2663b7a3c4ed5538bbcdb3bfeac98a03ac05d116
Christophe Guieu <christophe.guieu@gmail.com>
2014-04-11 16:13:46 +0200

    run.sh script now create word lists, relations and call the batch_importer

--- parser/omwg/run.sh
+++ parser/omwg/run.sh
@@ -12,3 +12,13 @@ then
 fi
 
 python directoryparser.py "$1"
+shift
+
+DB="db/omw-grap.db"
+NODES="csv_files/eng.csv"
+RELS="csv_files/rels-eng.csv"
+CP=""
+HEAP=4G
+for i in db/neo4j_batch_importer/lib/*.jar; do CP="$CP":"$i"; done
+#echo java -classpath $CP -Xmx$HEAP -Xms$HEAP -Dfile.encoding=UTF-8 org.neo4j.batchimport.Importer batch.properties "$DB" "$NODES" "$RELS" "$@"
+java -classpath $CP -Xmx$HEAP -Xms$HEAP -Dfile.encoding=UTF-8 org.neo4j.batchimport.Importer db/neo4j_batch_importer/batch.properties "$DB" "$NODES" "$RELS" "$@"
