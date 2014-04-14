f75ccdbee17dec50f50a9ea2e94976afa66671e9
adon <winy95battle@gmail.com>
2014-04-14 19:46:27 +0200

    Add hyper Function

--- parser/omwg/toolsomw.py
+++ parser/omwg/toolsomw.py
@@ -21,3 +21,13 @@ def writeLineRels(syn1, word1, syn2, word2, rel, lng, target):
 def writeLineWord(syn, word, lng, target):
     target.write("{syn}#{word}#{lng}\t{word}\n".format(
         syn=syn, word=word, lng=lng))
+
+def hyperRels(wn, hyper, lng, f):
+    for key in hyper.keys():
+        for g in wn[key]:
+            for g2 in hyper[key]:
+                for w in wn[g2]:
+                    g = str(g).replace(" ", "_")
+                    w = str(w).replace(" ", "_")
+                    writeLineRels(key, g, g2, w, 'HYPER', lng, f)
+                    writeLineRels(g2, w, key, g, 'HYPO', lng, f)
