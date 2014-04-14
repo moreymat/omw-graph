bf6e6d995e986d1a5d5b735c52a6c435067562d8
TALLOT Adrien <t1002534@tempo21-2.luminy.univ-amu.fr>
2014-04-14 13:53:01 +0200

    add \n + change filename

--- parser/omwg/toolsomw.py
+++ parser/omwg/toolsomw.py
@@ -6,18 +6,18 @@ __date__ = '14-04-2014'
 
 
 def writeHeaderRels(target):
-    target.write("name:string:key\tname:string:key\ttype")
+    target.write("name:string:key\tname:string:key\ttype\n")
 
 
 def writeHeaderWord(target):
-    target.write("name:string:key\tsynset\tword")
+    target.write("name:string:key\tsynset\tword\n")
 
 
 def writeLineRels(syn1, word1, syn2, word2, rel, lng, target):
-    target.write("{syn1}#{word1}#{lng}\t{syn2}#{word2}#{lng}\t{rel}".format(
+    target.write("{syn1}#{word1}#{lng}\t{syn2}#{word2}#{lng}\t{rel}\n".format(
         syn1=syn1, word1=word1, syn2=syn2, word2=word2, rel=rel, lng=lng))
 
 
 def writeLineWord(syn, word, lng, target):
-    target.write("{syn}#{word}#{lng}\t{word}".format(
+    target.write("{syn}#{word}#{lng}\t{word}\n".format(
         syn=syn, word=word, lng=lng))
