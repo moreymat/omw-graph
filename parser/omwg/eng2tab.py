748dab2a7938440c8dcd9ccfd61646ce6ec15a62
adon <winy95battle@gmail.com>
2014-04-15 14:07:55 +0200

    Improve processing time

--- parser/omwg/eng2tab.py
+++ parser/omwg/eng2tab.py
@@ -41,32 +41,49 @@ writeHeaderWord(outfile_word)
 #
 
 for s in w.all_synsets():
+    synset = createSynset(s.offset(), s.pos())
     for l in s.lemmas():
-        synset = createSynset(s.offset(), s.pos())
         writeLineWord(synset, l.name(), wnlang, outfile_word)
         for k in s.lemmas():
             if k != l:
                 writeLineRels(synset, l.name().replace('_',' '), synset, k.name().replace('_',' '), 'SYNO', wnlang, outfile_rels)
 
-##HYPERNYMS
-for s in w.all_synsets():
     hyper = s.hypernyms()
     lemmas = s.lemmas()
-    sysnet = createSynset(s.offset(), s.pos())
     for h in hyper:
         synsethyper = createSynset(h.offset(), h.pos())
         for lh in h.lemmas():
             for l in lemmas:
                 writeLineRels(synsethyper, lh.name().replace('_',' '), synset, l.name().replace('_',' '), 'HYPER', wnlang, outfile_rels)
 
-for s in w.all_synsets():
     hypo = s.hyponyms()
     lemmas = s.lemmas()
-    sysnet = createSynset(s.offset(), s.pos())
     for h in hypo:
         synsethypo = createSynset(h.offset(), h.pos())
         for lh in h.lemmas():
             for l in lemmas:
                 writeLineRels(synsethypo, lh.name().replace('_',' '), synset, l.name().replace('_',' '), 'HYPO', wnlang, outfile_rels)
 
+##HYPERNYMS
+#for s in w.all_synsets():
+#    hyper = s.hypernyms()
+#    lemmas = s.lemmas()
+#    synset = createSynset(s.offset(), s.pos())
+#    for h in hyper:
+#        synsethyper = createSynset(h.offset(), h.pos())
+#        for lh in h.lemmas():
+#            for l in lemmas:
+#                writeLineRels(synsethyper, lh.name().replace('_',' '), synset, l.name().replace('_',' '), 'HYPER', wnlang, outfile_rels)
+#
+##HYPONYMS
+#for s in w.all_synsets():
+#    hypo = s.hyponyms()
+#    lemmas = s.lemmas()
+#    synset = createSynset(s.offset(), s.pos())
+#    for h in hypo:
+#        synsethypo = createSynset(h.offset(), h.pos())
+#        for lh in h.lemmas():
+#            for l in lemmas:
+#                writeLineRels(synsethypo, lh.name().replace('_',' '), synset, l.name().replace('_',' '), 'HYPO', wnlang, outfile_rels)
+#
 outfile_rels.close()
