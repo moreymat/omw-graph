decca1618b4d10d41bd9a74686bc8d11de4d2bd1
Christophe Guieu <christophe.guieu@gmail.com>
2014-04-25 09:34:02 +0200

    add labels for lexical and synset nodes

--- omwg/srcs/toolsomw.py
+++ omwg/srcs/toolsomw.py
@@ -2,16 +2,11 @@
 #-*- coding: utf-8 -*-
 
 __author__ = 'Guieu Christophe, Tallot Adrien'
-__date__ = '14-04-2014'
+__date__ = '25-04-2014'
 
 
 def cleanWord(word):
     return word.replace("_", " ")
-
-
-def indexWord(word):
-    #return word.replace("_", "_")
-    return word
 
 
 def writeHeaderRels(target):
@@ -19,11 +14,11 @@ def writeHeaderRels(target):
 
 
 def writeHeaderWord(target):
-    target.write("name:string:lexkey\tlemma\tpartofspeech\n")
+    target.write("name:string:lexkey\tlemma\tpartofspeech\ttype:label\n")
 
 
 def writeHeaderSynset(target):
-    target.write("name:string:synkey\n")
+    target.write("name:string:synkey\ttype:label\n")
 
 
 def writeHeaderRelSynLex(target):
@@ -36,26 +31,17 @@ def writeLineRels(syn1, syn2, rel, target):
 
 
 def writeLineWord(lng, version, lex, lemma, pos, target):
-    target.write("{key}\t{lemma}\t{pos}\n".format(
+    target.write("{key}\t{lemma}\t{pos}\tLexicalEntry\n".format(
         key=lng + '-' + version + '-' + lex,
         lemma=lemma,
         pos=pos))
 
 
 def writeLineSynset(syn, target):
-    target.write("{syn}\n".format(syn=syn))
+    target.write("{syn}\tSynset\n".format(syn=syn))
 
 
 def writeLineRelSynLex(lex, lng, version, syn, target):
     target.write("{syn}\t{lex}\tlexical\n".format(
         syn=syn,
         lex=lng + '-' + version + '-' + lex))
-
-
-def hyperRels(wn, hyper, lng, f):
-    for key in hyper.keys():
-        for g in wn[key]:
-            for g2 in hyper[key]:
-                for w in wn[g2]:
-                    writeLineRels(key, g, g2, w, 'HYPER', lng, f)
-                    writeLineRels(g2, w, key, g, 'HYPO', lng, f)
